a375d9956ec5b72127c6cdd8156685fe8460768c
DESKTOP-7RBMLGI\Asus <anthony.romian2021@campus-eni.fr>
2021-09-06 21:03:41 +0200

    mise en place d envoi automatique fonctionnel

---
+++ src/Service/MailerService.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace App\Service;
+
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+
+class MailerService
+{
+
+    /**
+     * @var MailerService
+     */
+    private $mailer;
+
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    /**
+     * MailerService constructor
+     *
+     * @param MailerService $mailer
+     * @param Environment $twig
+     */
+    public function __construct (MailerInterface $mailer, Environment $twig){
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+
+    }
+
+    /**
+     * @param string $sujet
+     * @param string $from
+     * @param string $to
+     * @param string $template
+     * @param array $parameters
+     * @throws TransportExceptionInterface
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function send(string $sujet, string $from, string $to,string $template, array $parameters) : void
+    {
+        $email = (new Email())
+            ->from($from)
+            ->to($to)
+            ->subject($sujet)
+            ->html(
+                $this->twig->render($template, $parameters),
+                'text/html'
+            );
+
+
+
+        $this->mailer->send($email);
+
+    }
+
+
+
+}
