aea8d413ad2f12fe1ef84a13f18a7236ff6d3622
DESKTOP-7RBMLGI\Asus <anthony.romian2021@campus-eni.fr>
2021-10-05 13:42:41 +0200

    reglage apparence pour integration site

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,6 @@
-let ChauffageOk = document.getElementById("utilisateur_produit_vise_1");
-let ChauffageNok = document.getElementById("utilisateur_produit_vise_0");
-let Chauffage = document.getElementById("menu-deroulant");
+const ChauffageOk = document.getElementById("utilisateur_produit_vise_1");
+const ChauffageNok = document.getElementById("utilisateur_produit_vise_0");
+const Chauffage = document.getElementById("menu-deroulant");
 
 
 let div1 = document.getElementById("ElemChauffage1");
@@ -9,31 +9,31 @@ let div2 = document.getElementById("ElemChauffage2");
 ChauffageOk.addEventListener("click", () => {
 
     if(getComputedStyle(div1).display === "none"){
-        if (window.matchMedia("(min-width: 1024px)").matches) {
-            div1.style.display = "block";
-            div2.style.display = "block";
+        if (window.matchMedia("(min-width: 767px)").matches) {
+            div1.style.display = "flex";
+            div2.style.display = "flex";
             Chauffage.style.height = "24em"
-        } else if (window.matchMedia("(min-width: 738px)").matches) {
-            div1.style.display = "block";
-            div2.style.display = "block";
-            Chauffage.style.height = "24em"
+        } else if (window.matchMedia("(min-width: 460px)").matches) {
+            div1.style.display = "flex";
+            div2.style.display = "flex";
+            Chauffage.style.height = "28em"
         } else {
-            div1.style.display = "block";
-            div2.style.display = "block";
+            div1.style.display = "flex";
+            div2.style.display = "flex";
             Chauffage.style.height = "40em"
         }
 
     } else {
         ChauffageNok.addEventListener("click", () => {
-            if(getComputedStyle(div1).display === "block"){
-                if (window.matchMedia("(min-width: 1024px)").matches) {
+            if(getComputedStyle(div1).display === "flex"){
+                if (window.matchMedia("(min-width: 767px)").matches) {
                     div2.style.display = "none";
                     div1.style.display = "none";
-                    Chauffage.style.height = "6em"
-                } else if (window.matchMedia("(min-width: 738px)").matches) {
+                    Chauffage.style.height = "7em"
+                } else if (window.matchMedia("(min-width: 460px)").matches) {
                     div2.style.display = "none";
                     div1.style.display = "none";
-                    Chauffage.style.height = "6em"
+                    Chauffage.style.height = "8em"
                 } else {
                     div2.style.display = "none";
                     div1.style.display = "none";
@@ -43,26 +43,31 @@ ChauffageOk.addEventListener("click", () => {
         })
     }
     if(getComputedStyle(div2).display === "none"){
-        if (window.matchMedia("(min-width: 1024px)" && "(min-width: 738px)" ).matches) {
-            div1.style.display = "block";
-            div2.style.display = "block";
+        if (window.matchMedia("(min-width: 767px)").matches) {
+            div1.style.display = "flex";
+            div2.style.display = "flex";
             Chauffage.style.height = "24em"
+        } else if (window.matchMedia("(min-width: 460px)").matches) {
+            div1.style.display = "flex";
+            div2.style.display = "flex";
+            Chauffage.style.height = "28em"
         } else {
-            div1.style.display = "block";
-            div2.style.display = "block";
+            div1.style.display = "flex";
+            div2.style.display = "flex";
+            Chauffage.style.height = "40em"
             Chauffage.style.height = "40em"
         }
     } else {
         ChauffageNok.addEventListener("click", () => {
-            if(getComputedStyle(div2).display === "block"){
-                if (window.matchMedia("(min-width: 1024px)").matches) {
+            if(getComputedStyle(div2).display === "flex"){
+                if (window.matchMedia("(min-width: 767px)").matches) {
                     div2.style.display = "none";
                     div1.style.display = "none";
-                    Chauffage.style.height = "6em"
-                } else if( window.matchMedia("(min-width: 738px)").matches  ) {
+                    Chauffage.style.height = "7em"
+                } else if( window.matchMedia("(min-width: 460px)").matches  ) {
                     div2.style.display = "none";
                     div1.style.display = "none";
-                    Chauffage.style.height = "6em"
+                    Chauffage.style.height = "8em"
                 } else {
                     div2.style.display = "none";
                     div1.style.display = "none";
@@ -78,33 +83,29 @@ const slidePage = document.querySelector(".slide-page");
 const nextBtnFirst = document.querySelector(".firstNext");
 const prevBtnSec = document.querySelector(".prev-1");
 const submitBtn = document.querySelector(".submit");
-const progressText = document.querySelectorAll(".step p");
-const progressCheck = document.querySelectorAll(".step .check");
-const bullet = document.querySelectorAll(".step .bullet");
+const reframe_top = document.getElementById('reframe_top');
 let current = 1;
+
 
 nextBtnFirst.addEventListener("click", function(event){
     event.preventDefault();
     slidePage.style.display = "block";
     slidePage.style.marginLeft = "-25%";
-    bullet[current - 1].classList.add("active");
-    progressCheck[current - 1].classList.add("active");
-    progressText[current - 1].classList.add("active");
     current += 1;
+    setTimeout(function(){
+        reframe_top.scrollIntoView();
+    },500);
 });
 submitBtn.addEventListener("click", function(){
-    bullet[current - 1].classList.add("active");
-    progressCheck[current - 1].classList.add("active");
-    progressText[current - 1].classList.add("active");
     current += 1;
 });
 
 prevBtnSec.addEventListener("click", function(event){
     event.preventDefault();
     slidePage.style.marginLeft = "0%";
-    bullet[current - 2].classList.remove("active");
-    progressCheck[current - 2].classList.remove("active");
-    progressText[current - 2].classList.remove("active");
     current -= 1;
+    setTimeout(function(){
+        reframe_top.scrollIntoView();
+    },500);
 });
 
